b35f100d57811829912b09d4c9395ed48435132a
Leonardo Cintra <leonardo.ncintra@outlook.com>
2019-06-14 10:30:24 -0300

    2 tentativa heroku

--- src/main/resources/db/migration/mysql/V1.0__create_table_autor.sql
+++ src/main/resources/db/migration/mysql/V1.0__create_table_autor.sql
@@ -2,7 +2,7 @@ CREATE TABLE autor (
 	id bigint(20) NOT NULL AUTO_INCREMENT,
 	nome varchar(100) NOT NULL,
 	data_nascimento DATETIME NULL,
-	biografia TEXT null,
+	biografia TEXT NULL,
 	data_criacao DATETIME NOT NULL,
 	data_atualizacao DATETIME NOT NULL,
     PRIMARY KEY (id)
