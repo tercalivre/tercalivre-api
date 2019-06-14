6a3e7d06573c5be22124b67af9e09dd8a9828e64
Leonardo Cintra <leonardo.ncintra@outlook.com>
2019-06-14 10:21:52 -0300

    erro nao identificado

--- src/main/resources/db/migration/mysql/V1.0__create_table_autor.sql
+++ src/main/resources/db/migration/mysql/V1.0__create_table_autor.sql
@@ -1,9 +1,5 @@
 CREATE TABLE autor (
 	id bigint(20) NOT NULL AUTO_INCREMENT,
-	nome varchar(100) NOT NULL,
-	data_nascimento DATETIME NULL,
-	biografia TEXT null,
-	data_criacao DATETIME NOT NULL,
-	data_atualizacao DATETIME NOT NULL,
+	nome varchar(100) NOT NULL
     PRIMARY KEY (id)
 );
