fc6717353f3516479c9532542e53e685aecf885e
Leonardo Cintra <leonardo.ncintra@outlook.com>
2019-06-14 10:25:19 -0300

    vendo se eh o url datasource

--- src/main/resources/db/migration/mysql/V1.0__create_table_autor.sql
+++ src/main/resources/db/migration/mysql/V1.0__create_table_autor.sql
@@ -1,5 +1,9 @@
 CREATE TABLE autor (
 	id bigint(20) NOT NULL AUTO_INCREMENT,
 	nome varchar(100) NOT NULL,
+	data_nascimento DATETIME NULL,
+	biografia TEXT null,
+	data_criacao DATETIME NOT NULL,
+	data_atualizacao DATETIME NOT NULL,
     PRIMARY KEY (id)
 );
